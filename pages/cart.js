87bafb80a12ee401c1688dd2394356b113d46b4a
usama.malik <usama.malik93@hotmail.com>
2023-01-06 15:34:36 +0500

    js cookie to store state on refresh

--- pages/cart.js
+++ pages/cart.js
@@ -5,8 +5,9 @@ import Layout from "../components/Layout";
 import { Store } from "../utils/Store";
 import { XCircleIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 
-export default function CartScreen() {
+function CartScreen() {
   const router = useRouter();
   const { state, dispatch } = useContext(Store);
 
@@ -102,3 +103,5 @@ export default function CartScreen() {
     </Layout>
   );
 }
+
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
